5796d152088b4e10fab8cdcd9cdc29238b799373
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-08-03 16:38:06 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro firulais = new Perro();
+
+            firulais.saludar();
+
+
+
+            Console.WriteLine(firulais.nombre);
+            Console.WriteLine($"RAZA:{firulais.raza}");
+               
+            Perro max = new Perro("Max");
+            Console.WriteLine(max.nombre);
+        }
+    }
+}
+    
